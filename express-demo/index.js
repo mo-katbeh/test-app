0585237731a90717f4374626a3536039a11a24fd
mo-katbeh <hamode.katbeh@gmail.com>
2025-06-17 19:24:35 +0300

    End of  Building RESTful API's Using Express

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -18,11 +19,15 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res)=>{
     const course = courses.find((c) => c.id === parseInt(req.params.id)) 
-    if (!course) res.status(400).send('The course was not found');
+    if (!course) res.status(404).send('The course was not found');
     res.send(course)
 });
 
 app.post('/api/courses', (req, res) => {
+         
+    const {error} = validateCourse(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
     const course ={
         id: courses.length + 1,
         name: req.body.name
@@ -30,6 +35,37 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) =>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send("There's no course with this id!!")
+    
+    const {error} = validateCourse(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course)
+
+});
+
+app.delete('/api/courses/:id', (req, res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send("There's no course with this id!!");
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.send(course);
+
+});
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(course);
+}
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);

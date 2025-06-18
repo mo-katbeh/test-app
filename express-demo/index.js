969aaedbf751c2d63fd08e93dd785bcfac488cc1
mo-katbeh <hamode.katbeh@gmail.com>
2025-06-18 10:23:06 +0300

    type of middleware

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,7 +1,19 @@
+const helmet = require('helmet');
+const morgan = require('morgan');
 const Joi = require('joi');
 const express = require('express');
+const logger = require('./logger');
+const authenticator = require('./authenticating');
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
+// app.use(logger);
+// app.use(authenticator);
+
 const port = process.env.PORT || 3000;
 
 const courses = [

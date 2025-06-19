05cdc4c20b62d6737c98bcbbb0ab7246d9efa48b
mo-katbeh <hamode.katbeh@gmail.com>
2025-06-19 10:52:07 +0300

    Restructure test app

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,82 +1,41 @@
+const config = require('config');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+
 const Joi = require('joi');
 const express = require('express');
-const logger = require('./logger');
-const authenticator = require('./authenticating');
+const logger = require('./middleware/logger');
+const authenticator = require('./middleware/authenticating');
+const courses = require('./routes/courses');
+const home = require('./routes/home');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(helmet());
-app.use(morgan('tiny'));
+app.use('/api/courses', courses);
+app.use('/home', home);
+
+// console.log('Application Name:' + config.get('name'));
+// console.log('Mail Server:' + config.get('mail.host'));
+// console.log('Mail password:' + config.get('mail.password'));
+
+if (app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    startupDebugger('Morgan enabled..')
+}
+
+dbDebugger('DB is working');
+
+// console.log(`NODE_ENV = ${process.env.NODE_ENV}`);
+// console.log(`NODE_ENV = ${app.get('env')}`);
 // app.use(logger);
 // app.use(authenticator);
 
 const port = process.env.PORT || 3000;
-
-const courses = [
-    {id:1, name:'course1'},
-    {id:2, name:'course2'},
-    {id:3, name:'course3'},
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello world');
-});
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-});
-
-app.get('/api/courses/:id', (req, res)=>{
-    const course = courses.find((c) => c.id === parseInt(req.params.id)) 
-    if (!course) res.status(404).send('The course was not found');
-    res.send(course)
-});
-
-app.post('/api/courses', (req, res) => {
-         
-    const {error} = validateCourse(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    const course ={
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-});
-
-app.put('/api/courses/:id', (req, res) =>{
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send("There's no course with this id!!")
-    
-    const {error} = validateCourse(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    course.name = req.body.name;
-    res.send(course)
-
-});
-
-app.delete('/api/courses/:id', (req, res)=>{
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send("There's no course with this id!!");
-
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-    res.send(course);
-
-});
-
-function validateCourse(course){
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-
-    return schema.validate(course);
-}
 
 
 app.listen(port, () => {
--- express-demo/middleware/logger.js
+++ express-demo/middleware/logger.js
@@ -2,5 +2,4 @@ function log(req, res, next){
     console.log('Logging...');
     next();
 }
-
 module.exports = log;
